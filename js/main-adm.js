73164a8d8ba6c9a80f84e42e369e4809b0bf8b98
Leonardo Henrique De Oliveira Marchioro <leonardo.marchioro@linx.com.br>
2024-09-12 17:43:48 -0300

    Envio de hoje. Terminei os produto. bom... uma parte deles..

--- js/main-adm.js
+++ js/main-adm.js
@@ -38,9 +38,10 @@ function tratarErro(error) {
 
 // Função para tratar os dados recebidos da API
 function processarDados(data) {
-    console.log(data.length);
+    let dados = data.length;
+    console.log(dados);
     let productsValue = document.getElementById('productsValue');
-    productsValue.innerHTML = data.length;
+    productsValue.innerHTML = dados;
 
 }
 
@@ -49,20 +50,134 @@ const box = document.querySelectorAll('.box');
 const botaoVoltar = document.querySelector('.titleVoltar');
 const rowInformations = document.querySelector('.row-informations');
 
+function buscarProdutos() {
+    const urlApi = 'https://fakestoreapi.com/products';
+
+    fetch(urlApi)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Erro na requisição: ' + response.status);
+            }
+
+            return response.json(); // Converte a resposta em JSON
+
+        })
+        .then(processarDados) // Chama a função processarDados com os dados
+        .catch(tratarErro);
+
+    function tratarErro(error) {
+        console.error('Erro ao consumir a API:', error);
+    }
+
+
+    function processarDados(data) {
+        console.log(data);
+        const tables = document.querySelector('.tables');
+        tables.classList.add('active');
+
+        let tableContent = `
+        <table class="table table-hover table-danger col-5">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col" class="email">Valor</th>
+                    <th scope="col">Categoria</th>
+                    <th scope="col" class="acoes">Ações</th>
+                </tr>
+            </thead>
+            <tbody>`;
+
+        data.forEach((data) => {
+            tableContent += `
+                <tr class="tr">
+                    <th scope="row" class="idProduto">${data.id}</th>
+                    <td class="user">${data.title}</td>
+                    <td class="email"> R$ ${data.price}</td>
+                    <td class="email">${data.category}</td>
+                 <td class="buttonDelete" id="produtoDelete">Deletar</td>
+                </tr>
+            `;
+
+
+        });
+
+        tableContent += `
+            </tbody>
+        </table>`;
+
+
+        tables.innerHTML = tableContent;
+        
+        document.querySelectorAll('#produtoDelete').forEach(element =>{
+            element.addEventListener('click', (e)=>{
+                let row = e.target.closest('tr');
+                
+                let id = row.querySelector('.idProduto');
+                let idProduto = id.textContent;
+                
+
+                removerProduto(idProduto);
+                row.remove();
+                
+                
+            })
+        })
+        
+
+    }
+
+  
+
+}
+
+
+function removerProduto(productId) {
+    fetch(`${urlApi}/${productId}`, {
+        method: 'DELETE'
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Erro ao remover o produto: ' + response.status);
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('Produto removido:', data);
+            
+        })
+        .catch(error => {
+            console.error('Erro:', error);
+        });
+
+       
+
+}
 
 
 box.forEach(element => {
     element.addEventListener('click', () => {
+
         const boxes = document.getElementsByClassName('boxes');
+        const botaoCadastrar = document.getElementsByClassName('titleCadastrar');
+        const tables = document.querySelector('.tables');
+        console.log(botaoCadastrar)
         console.log(element.id);
 
-        if (element.id === 'idUser') {
-            boxes[0].classList.add('disabled');
-            rowInformations.classList.add('active');
+        //////////////////////// usuarios /////////////////////////////////
+        //////////////////////// usuarios /////////////////////////////////
+        //////////////////////// usuarios /////////////////////////////////
 
+        if (element.id === 'idUser') {
+
+            boxes[0].classList.add('disabled');
+            botaoVoltar.classList.add('active');
+            tables.classList.add('active');
+            botaoCadastrar[0].classList.remove('active');
+            rowInformations.classList.add('active')
             const users = JSON.parse(localStorage.getItem('usuarios')) || [];
 
-            const tables = document.querySelector('.tables');
+
 
             let tableContent = `
             <table class="table table-hover table-danger col-5">
@@ -102,6 +217,7 @@ box.forEach(element => {
 
             if (buttonDelete) {
                 buttonDelete.forEach(button => {
+                    
                     button.addEventListener('click', (e) => {
                         const row = e.target.closest('tr'); // 'tr' onde o botão foi clicado
                         console.log(row)
@@ -122,15 +238,46 @@ box.forEach(element => {
             }
 
         }
+        //////////////////////// produtos /////////////////////////////////
+        //////////////////////// produtos /////////////////////////////////
+        //////////////////////// produtos /////////////////////////////////
+        if (element.id === 'idProducts') {
+            boxes[0].classList.add('disabled');
+            botaoVoltar.classList.add('active');
+            botaoCadastrar[0].classList.add('active');
+            rowInformations.classList.add('active');
+
+            buscarProdutos();
+
+        }
+
+        if (element.id === 'idCostumers') {
+            boxes[0].classList.add('disabled');
+            botaoVoltar.classList.add('active');
+            botaoCadastrar[0].classList.add('active');
+            rowInformations.classList.add('active');
+
+            
+
+        }
+
+
+
     });
 });
 
 
 botaoVoltar.addEventListener('click', () => {
     const boxes = document.getElementsByClassName('boxes');
+    const botaoCadastrar = document.getElementsByClassName('titleCadastrar');
+    const tables = document.querySelector('.tables');
 
     boxes[0].classList.remove('disabled');
+    botaoCadastrar[0].classList.remove('active');
+    botaoVoltar.classList.remove('active');
+    tables.classList.remove('active');
     rowInformations.classList.remove('active');
+
 
 })
 
